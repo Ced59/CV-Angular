2c91ff40281a5a0f23765395190a96867e0db5fe
Cedric <c.caudron59@gmail.com>
2020-06-15 15:21:18 +0200

    Age calc fixed

--- src/app/services/age.service.ts
+++ src/app/services/age.service.ts
@@ -23,7 +23,10 @@ export class AgeService {
       let diffDuration = moment.duration(moment().diff(moment([1984, 10, 2, 0, 8, 0])));
 
       this.age.year = diffDuration.years();
-      this.age.month = diffDuration.months();
+      this.age.month = moment().diff(moment([1984, 10, 2, 0, 8, 0]), 'months') % 12 + 1;
+      if (this.age.month >= 12) {
+        this.age.month = 0;
+      }
       this.age.days = diffDuration.days();
       this.age.hours = diffDuration.hours();
       this.age.minutes = diffDuration.minutes();
